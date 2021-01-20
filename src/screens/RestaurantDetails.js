e6d8d088e099aea6d1166910cdde86a9fa68d861
Jaquan1314 <javaughngrant@gmail.com>
2021-01-20 13:28:32 -0500

    Restaurant details screen shows approtiate info. More to come

---
+++ src/screens/RestaurantDetails.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RestaurantDetails = ({ route, navigation }) => {
+  console.log('REVIEW DETAILS');
+  const {
+    image_url,
+    location,
+    name,
+    phone,
+    photos,
+    rating,
+    url,
+  } = route.params;
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text>{location}</Text>
+    </View>
+  );
+};
+
+export default RestaurantDetails;
