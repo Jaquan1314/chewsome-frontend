f816e7fa81816442bf33fa5f98f817e4f3ca4b10
Jaquan1314 <javaughngrant@gmail.com>
2021-01-20 13:27:30 -0500

    Home screen renders Restaurant cards through Flatlist component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, FlatList, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { fetchRestaurants } from '../redux/actions/index';
@@ -7,32 +7,18 @@ import RestaurantCard from '../Component/RestaurantCard';
 
 const Home = (props) => {
   useEffect(() => {
-    console.log('INSIDE HOME COMPONENT');
+    console.log('INSIDE HOME COMPONENT', props);
     props.fetchRestaurants();
   }, []);
-  // console.log('OUTSIDE OF USEEFFECT', props.restaurants[0].name);
-  // const renderRestaurants = () => {
-  //   return props.restaurants.map((restaurant) => (
-  //     <RestaurantCard
-  //       key={restaurant.id}
-  //       restaurantObj={restaurant}
-  //       image={restaurant.image}
-  //       location={restaurant.location}
-  //       name={restaurant.name}
-  //       photos={restaurant.photos}
-  //       phone={restaurant.phone}
-  //       rating={restaurant.rating}
-  //       url={restaurant.url}
-  //     />
-  //   ));
-  // };
 
   return (
     <SafeAreaView style={''}>
       <Text>Home Component</Text>
       <FlatList
         data={props.restaurants}
-        renderItem={({ item }) => <RestaurantCard item={item} />}
+        renderItem={({ item }) => (
+          <RestaurantCard item={item} navigation={props.navigation} />
+        )}
       />
       {/* <Text>{renderRestaurants()}</Text> */}
     </SafeAreaView>
