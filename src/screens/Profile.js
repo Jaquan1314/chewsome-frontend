fd8ad5c5a38023cabd841c734bb337cce83b407d
Jaquan1314 <javaughngrant@gmail.com>
2021-01-20 14:24:48 -0500

    logOut not working as it should, will fix later

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -5,15 +5,17 @@ import { connect } from 'react-redux';
 import { logOut } from '../redux/actions/index';
 
 const Profile = (props) => {
+  // console.log('PROFILE PROPS', props);
   return (
     <View style={globalStyles.container}>
-      <Text>Username</Text>
-      <Text>Email</Text>
+      <Text>Username: {props.user.username}</Text>
+      <Text>Email: {props.user.email}</Text>
       <TouchableOpacity
         style={globalStyles.logOutButton}
         onPress={() => {
-          props.logOut();
           props.navigation.navigate('Welcome');
+          props.logOut();
+          // console.log('USER LOGGED OUT', props.user);
         }}
       >
         <Text style={globalStyles.buttonText}>LogOut</Text>
