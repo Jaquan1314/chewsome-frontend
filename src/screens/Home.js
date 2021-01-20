0cfbc075f684dcdb120d3547ec37530bd75a0e55
Jaquan1314 <javaughngrant@gmail.com>
2021-01-20 00:25:44 -0500

    refacted code to utilize Flatlist to gather and render items

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchRestaurants } from '../redux/actions/index';
+import { globalStyles } from '../../globalStyles';
+import RestaurantCard from '../Component/RestaurantCard';
 
-const Home = () => {
+const Home = (props) => {
+  useEffect(() => {
+    console.log('INSIDE HOME COMPONENT');
+    props.fetchRestaurants();
+  }, []);
+  // console.log('OUTSIDE OF USEEFFECT', props.restaurants[0].name);
+  // const renderRestaurants = () => {
+  //   return props.restaurants.map((restaurant) => (
+  //     <RestaurantCard
+  //       key={restaurant.id}
+  //       restaurantObj={restaurant}
+  //       image={restaurant.image}
+  //       location={restaurant.location}
+  //       name={restaurant.name}
+  //       photos={restaurant.photos}
+  //       phone={restaurant.phone}
+  //       rating={restaurant.rating}
+  //       url={restaurant.url}
+  //     />
+  //   ));
+  // };
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={''}>
       <Text>Home Component</Text>
-    </View>
+      <FlatList
+        data={props.restaurants}
+        renderItem={({ item }) => <RestaurantCard item={item} />}
+      />
+      {/* <Text>{renderRestaurants()}</Text> */}
+    </SafeAreaView>
   );
 };
 
-export default Home;
+const msp = (state) => {
+  return {
+    restaurants: state.restaurants,
+  };
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const mdp = (dispatch) => {
+  return {
+    fetchRestaurants: () => dispatch(fetchRestaurants()),
+  };
+};
+
+export default connect(msp, mdp)(Home);
