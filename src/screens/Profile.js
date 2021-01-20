5926475aca7f18ad5f4946d6069ccd3b66e6c324
Jaquan1314 <javaughngrant@gmail.com>
2021-01-19 19:44:22 -0500

    Worked on user having the ability to logout of the application

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { globalStyles } from '../../globalStyles';
+import { connect } from 'react-redux';
+import { logOut } from '../redux/actions/index';
 
-const Profile = () => {
+const Profile = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Profile Component</Text>
+    <View style={globalStyles.container}>
+      <Text>Username</Text>
+      <Text>Email</Text>
+      <TouchableOpacity
+        style={globalStyles.logOutButton}
+        onPress={() => {
+          props.logOut();
+          props.navigation.navigate('Welcome');
+        }}
+      >
+        <Text style={globalStyles.buttonText}>LogOut</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default Profile;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const msp = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mdp = (dispatch) => {
+  return {
+    logOut: () => dispatch(logOut()),
+  };
+};
+
+export default connect(msp, mdp)(Profile);
