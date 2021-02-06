dc59f55ed9ec3d4a2f228d522d7a1c028a9848bf
Jaquan1314 <javaughngrant@gmail.com>
2021-02-06 12:12:12 -0500

    Destructed props, adding removing functionality next

--- src/Component/FavoriteCard.js
+++ src/Component/FavoriteCard.js
@@ -1,38 +1,23 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-} from 'react-native';
+import { Text } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 
-const FavoriteCard = (props) => {
-  console.log('INSIDE FAV CARD', props);
-  const { id, restaurant, user } = props;
+const FavoriteCard = ({ favoriteObj }) => {
+  console.log('INSIDE FAV CARD', favoriteObj);
+  const { id, restaurant, user } = favoriteObj;
   return (
-    <Card key={id} containerStyle={{ width: '80%' }}>
+    <Card containerStyle={{ width: '80%' }}>
       <Card.Title>{restaurant.name}</Card.Title>
       <Card.Divider />
       <Card.Image source={{ uri: restaurant.image_url }} />
       <Text>{restaurant.location}</Text>
       <Text>{restaurant.phone}</Text>
-      <Button title="Remove from favorite" />
+      <Button
+        title="Remove from favorite"
+        onPress={() => console.log("You're clicking me senpai")}
+      />
     </Card>
-    // <TouchableOpacity style={styles.card}>
-    //   <Text>{restaurant.name}</Text>
-    //   <Text>{restaurant.location}</Text>
-    //   <Text>{restaurant.phone}</Text>
-    // </TouchableOpacity>
   );
 };
 
 export default FavoriteCard;
-
-const styles = StyleSheet.create({
-  container: {
-    // flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-});
