b63536f28bde10a047c87c091f127d5188762025
Jaquan1314 <javaughngrant@gmail.com>
2021-02-15 16:22:51 -0500

    react native LogBox added to eliminate yellow warnings

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import LandingStack from './src/routes/LandingStack';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
+import { LogBox } from 'react-native';
 import store from './src/redux/store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+LogBox.ignoreAllLogs();
 
 function App() {
   return (
